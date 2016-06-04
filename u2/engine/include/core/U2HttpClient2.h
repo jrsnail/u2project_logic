c95fdc979a0979a400ae770d0cca09a7e94a0122
jrsnail <jr19841227@gmail.com>
2016-06-04 11:00:35 +0800

    + 添加HttpClient2；

---
+++ u2/engine/include/core/U2HttpClient2.h
@@ -0,0 +1,277 @@
+﻿#ifndef __U2HttpClient2_H__
+#define __U2HttpClient2_H__
+
+#include "U2Prerequisites.h"
+#include "U2STLRedefined.h"
+#include "threading/U2ThreadHeaders.h"
+#include "U2Singleton.h"
+#include "U2Task.h"
+#include "U2TaskLoop.h"
+#include "U2HeaderPrefix.h"
+
+
+U2EG_NAMESPACE_BEGIN
+
+
+class _U2Export HttpRequest : public Task
+{
+public:
+	/**
+	* The HttpProtocol type enum.
+	*/
+	enum class Type
+	{
+		HTTP_GET,
+		HTTP_POST,
+		HTTP_PUT,
+		HTTP_DELETE,
+		HTTP_UNKNOWN,
+	};
+
+public:
+    HttpRequest(const String& type, const String& name)
+		: Task(type, name)
+	{}
+	virtual ~HttpRequest() {};
+
+// 	virtual void preRecycleByPool() override;
+// 	virtual void postReuseFromPool() override;
+
+	virtual bool canConcate(const HttpRequest* request);
+
+	void setUrl(const String& url) { m_szUrl = url; };
+	const String& getUrl() const { return m_szUrl; };
+
+	void setHttpType(const HttpRequest::Type& type) { m_eType = type; };
+	const HttpRequest::Type getHttpType() const { return m_eType; };
+
+	/** Set custom-defined headers.
+	@param pHeaders the string vector of custom-defined headers.
+	*/
+	void setHttpHeaders(const vector<String>::type& headers)
+	{
+		m_Headers = headers;
+	}
+
+	/** Get custom headers.
+	@return std::vector<std::string> the string vector of custom-defined headers.
+	*/
+	const vector<String>::type& getHttpHeaders() const
+	{
+		return m_Headers;
+	}
+
+    /** Get the request data pointer of HttpRequest object.
+    @return char* the request data pointer.
+    */
+    inline const u2char* const getData();
+
+    /** Get the size of request data
+    @return ssize_t the size of request data
+    */
+    inline size_t getDataSize();
+
+
+    virtual void run() override;
+
+protected:
+	String		m_szUrl;
+	Type		m_eType;
+	vector<String>::type	m_Headers;
+    vector<u2char>::type	m_Data;
+};
+
+
+class _U2Export HttpResponse : public Task
+{
+public:
+    HttpResponse(const String& type, const String& name);
+    virtual ~HttpResponse();
+
+//     virtual void preRecycleByPool() override;
+//     virtual void postReuseFromPool() override;
+
+    /** Set response headers.
+    @param pHeaders the string vector of response headers.
+    */
+    inline void setHttpHeader(const vector<u2char>::type& data)
+    {
+        m_Headers = data;
+    }
+
+    /** Get response headers.
+    @return std::vector<std::string> the string vector of response headers.
+    */
+    inline vector<u2char>::type* getHttpHeader()
+    {
+        return &m_Headers;
+    }
+
+    /** Set the http response code.
+    @param value the http response code that represent whether the request is successful or not.
+    */
+    inline void setResultCode(long value)
+    {
+        m_lResultCode = value;
+    }
+
+    /** Get the http response code to judge whether response is successful or not.
+    I know that you want to see the _responseCode is 200.
+    If _responseCode is not 200, you should check the meaning for _responseCode by the net.
+    @return long the value of _responseCode
+    */
+    inline u2int64 getResultCode()
+    {
+        return m_lResultCode;
+    }
+
+    /** Set the error buffer which will tell you more the reason why http request failed.
+    @param value a string pointer that point to the reason.
+    */
+    inline void setErrorBuffer(const char* value)
+    {
+        m_szErrorBuffer.clear();
+        m_szErrorBuffer.assign(value);
+    }
+
+    /** Get the error buffer which will tell you more about the reason why http request failed.
+    @return const char* the pointer that point to _errorBuffer.
+    */
+    inline const String& getErrorBuffer()
+    {
+        return m_szErrorBuffer;
+    }
+
+    virtual void run() override;
+
+protected:
+    vector<u2char>::type	m_Headers;
+    u2int64                 m_lResultCode;   //< the status code returned from libcurl, e.g. 200, 404
+    String					m_szErrorBuffer;   //< if _responseCode != 200, please read _errorBuffer to find the reason
+};
+
+
+class HttpRelay : public PostTaskAndReplyRelay
+{
+public:
+    HttpRelay(const String& type, const String& name);
+    virtual ~HttpRelay();
+
+    inline HttpRequest* request();
+    inline HttpResponse* response();
+};
+
+
+class _U2Export HttpTaskLoop : public TaskLoop
+{
+public:
+    explicit HttpTaskLoop(const String& type, const String& name);
+    virtual ~HttpTaskLoop();
+
+    virtual void postTask(Task* task) override;
+
+    virtual void postTaskAndReply(Task* task, Task* reply) override;
+
+    virtual void run() override;
+
+    virtual void quit() override;
+
+    /**
+    * Set the timeout value for connecting.
+    *
+    * @param value the timeout value for connecting.
+    */
+    void setTimeoutForConnect(size_t value);
+
+    /**
+    * Get the timeout value for connecting.
+    *
+    * @return int the timeout value for connecting.
+    */
+    size_t getTimeoutForConnect();
+
+    /**
+    * Set the timeout value for reading.
+    *
+    * @param value the timeout value for reading.
+    */
+    void setTimeoutForRead(size_t value);
+
+    /**
+    * Get the timeout value for reading.
+    *
+    * @return int the timeout value for reading.
+    */
+    size_t getTimeoutForRead();
+
+    /**
+    * Set root certificate path for SSL verification.
+    *
+    * @param caFile a full path of root certificate.if it is empty, SSL verification is disabled.
+    */
+    void setSSLVerification(const String& caFile);
+
+    /**
+    * Get ths ssl CA filename
+    *
+    * @return the ssl CA filename
+    */
+    const String& getSSLVerification();
+
+    /**
+    * Enable cookie support.
+    *
+    * @param cookieFile the filepath of cookie file.
+    */
+    void enableCookies(const String& cookieFile);
+
+    /**
+    * Get the cookie filename
+    *
+    * @return the cookie filename
+    */
+    const String& getCookieFilename();
+
+protected:
+    void _runInternal();
+
+    void _addToIncomingQueue(Task* task);
+
+protected:
+    std::thread     m_thread;
+
+    // Protect access to m_IncomingQueue.
+    U2_MUTEX(m_mtxIncomingQueue);
+    U2_THREAD_SYNCHRONISER(m_IncomingQueueSync);
+    // A null terminated list which creates an incoming_queue of tasks that are
+    // acquired under a mutex for processing on this instance's thread. These
+    // tasks have not yet been sorted out into items for our work_queue_ vs items
+    // that will be handled by the TimerManager.
+    std::queue<Task*> m_IncomingQueue;
+
+    // This flag is set to false when Run should return.
+    bool m_bKeepRunning;
+
+
+    U2_MUTEX(m_TimeoutForConnectMutex);
+    size_t m_uTimeoutForConnect;
+
+    U2_MUTEX(m_TimeoutForReadMutex);
+    size_t m_uTimeoutForRead;
+
+    U2_MUTEX(m_SslCaFilenameMutex);
+    String m_szSslCaFilename;
+
+    U2_MUTEX(m_CookieFilenameMutex);
+    String m_szCookieFilename;
+    
+};
+
+
+
+#include "U2HeaderSuffix.h"
+
+U2EG_NAMESPACE_END
+
+
+#endif // __U2TypedObjectManager_H__
---
+++ u2/engine/src/core/U2HttpClient2.cpp
@@ -0,0 +1,483 @@
+﻿#include "U2HttpClient2.h"
+
+#include <curl/curl.h>
+#include "U2LogManager.h"
+#include "U2Exception.h"
+#include "U2NetMsgCenter.h"
+#include "U2FrameListenerCollection.h"
+#include "U2StreamQueue.h"
+#include "U2DataFilterStream.h"
+#include "U2StringStream.h"
+
+
+U2EG_NAMESPACE_USING
+
+
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+// void HttpRequest::preRecycleByPool()
+// {
+// 	m_szUrl = BLANK;
+// 	m_eType = HttpRequest::Type::HTTP_UNKNOWN;
+// 	m_Headers.clear();
+// 
+// 	OutMsg::preRecycleByPool();
+// }
+//-----------------------------------------------------------------------
+// void HttpRequest::postReuseFromPool()
+// {
+// 	OutMsg::postReuseFromPool();
+// 
+// 	m_szUrl = BLANK;
+// 	m_eType = HttpRequest::Type::HTTP_UNKNOWN;
+// 	m_Headers.clear();
+// }
+//-----------------------------------------------------------------------
+bool HttpRequest::canConcate(const HttpRequest* request)
+{
+	do 
+	{
+		const HttpRequest* pOther = dynamic_cast<const HttpRequest*>(request);
+		if (pOther == nullptr)
+		{
+			break;
+		}
+// 		if (this->getMsgType() != MT_PREDICTABLE && this->getMsgType() != MT_SYNCHRONOUS)
+// 		{
+// 			break;
+// 		}
+// 		if (this->getMsgType() != pOther->getMsgType())
+// 		{
+// 			break;
+// 		}
+		if (this->getUrl() != pOther->getUrl())
+		{
+			break;
+		}
+		if (this->getHttpType() != pOther->getHttpType())
+		{
+			break;
+		}
+		if (this->getHttpHeaders() != pOther->getHttpHeaders())
+		{
+			break;
+		}
+		return true;
+
+	} while (false);
+
+	return false;
+}
+//-----------------------------------------------------------------------
+const u2char* const HttpRequest::getData()
+{
+    if (m_Data.size() != 0)
+        return &(m_Data.front());
+
+    return nullptr;
+}
+//-----------------------------------------------------------------------
+size_t HttpRequest::getDataSize()
+{
+    return m_Data.size();
+}
+//-----------------------------------------------------------------------
+void HttpRequest::run()
+{
+
+}
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+HttpResponse::HttpResponse(const String& type, const String& name)
+    : Task(type, name)
+    , m_lResultCode(0L)
+{
+}
+//-----------------------------------------------------------------------
+HttpResponse::~HttpResponse()
+{
+}
+//-----------------------------------------------------------------------
+// void HttpResponse::preRecycleByPool()
+// {
+//     m_Headers.clear();
+//     m_lResultCode = 0L;
+//     m_szErrorBuffer = BLANK;
+// 
+//     InMsg::preRecycleByPool();
+// }
+//-----------------------------------------------------------------------
+// void HttpResponse::postReuseFromPool()
+// {
+//     InMsg::postReuseFromPool();
+// 
+//     m_Headers.clear();
+//     m_lResultCode = 0L;
+//     m_szErrorBuffer = BLANK;
+// }
+//-----------------------------------------------------------------------
+void HttpResponse::run()
+{
+
+}
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+HttpRelay::HttpRelay(const String& type, const String& name)
+    : PostTaskAndReplyRelay(type, name)
+{
+}
+//-----------------------------------------------------------------------
+HttpRelay::~HttpRelay()
+{
+}
+//-----------------------------------------------------------------------
+HttpRequest* HttpRelay::request()
+{
+    return dynamic_cast<HttpRequest*>(m_pTask);
+}
+//-----------------------------------------------------------------------
+HttpResponse* HttpRelay::response()
+{
+    return dynamic_cast<HttpResponse*>(m_pReply);
+}
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+//Configure curl's timeout property
+static bool configureCURL(HttpTaskLoop* client, CURL* handle, char* errorBuffer)
+{
+    if (!handle) {
+        return false;
+    }
+
+    int32_t code;
+    code = curl_easy_setopt(handle, CURLOPT_ERRORBUFFER, errorBuffer);
+    if (code != CURLE_OK) {
+        return false;
+    }
+    code = curl_easy_setopt(handle, CURLOPT_TIMEOUT, client->getTimeoutForRead());
+    if (code != CURLE_OK) {
+        return false;
+    }
+    code = curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT, client->getTimeoutForConnect());
+    if (code != CURLE_OK) {
+        return false;
+    }
+
+    std::string sslCaFilename = client->getSSLVerification();
+    if (sslCaFilename.empty()) {
+        curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 0L);
+        curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 0L);
+    }
+    else {
+        curl_easy_setopt(handle, CURLOPT_SSL_VERIFYPEER, 1L);
+        curl_easy_setopt(handle, CURLOPT_SSL_VERIFYHOST, 2L);
+        curl_easy_setopt(handle, CURLOPT_CAINFO, sslCaFilename.c_str());
+    }
+
+    // FIXED #3224: The subthread of CCHttpClient interrupts main thread if timeout comes.
+    // Document is here: http://curl.haxx.se/libcurl/c/curl_easy_setopt.html#CURLOPTNOSIGNAL 
+    curl_easy_setopt(handle, CURLOPT_NOSIGNAL, 1L);
+
+    curl_easy_setopt(handle, CURLOPT_ACCEPT_ENCODING, "");
+
+    return true;
+}
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+typedef size_t(*write_callback)(void *ptr, size_t size, size_t nmemb, void *stream);
+//-----------------------------------------------------------------------
+// Callback function used by libcurl for collect response data
+static size_t writeData(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+    std::vector<char> *recvBuffer = (std::vector<char>*)stream;
+    size_t sizes = size * nmemb;
+
+    // add data to the end of recvBuffer
+    // write data maybe called more than once in a single request
+    recvBuffer->insert(recvBuffer->end(), (char*)ptr, (char*)ptr + sizes);
+
+    return sizes;
+}
+//-----------------------------------------------------------------------
+// Callback function used by libcurl for collect header data
+static size_t writeHeaderData(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+    std::vector<char> *recvBuffer = (std::vector<char>*)stream;
+    size_t sizes = size * nmemb;
+
+    // add data to the end of recvBuffer
+    // write data maybe called more than once in a single request
+    recvBuffer->insert(recvBuffer->end(), (char*)ptr, (char*)ptr + sizes);
+
+    return sizes;
+}
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+class CURLRaii
+{
+private:
+    /// Instance of CURL
+    CURL *m_pCurl;
+    /// Keeps custom header data
+    curl_slist *m_pHeaders;
+
+public:
+    CURLRaii()
+        : m_pCurl(curl_easy_init())
+        , m_pHeaders(nullptr)
+    {
+    }
+
+    ~CURLRaii()
+    {
+        if (m_pCurl)
+            curl_easy_cleanup(m_pCurl);
+        /* free the linked list for header data */
+        if (m_pHeaders)
+            curl_slist_free_all(m_pHeaders);
+    }
+
+    template <class T>
+    bool setOption(CURLoption option, T data)
+    {
+        return CURLE_OK == curl_easy_setopt(m_pCurl, option, data);
+    }
+
+    /**
+    * @brief Inits CURL instance for common usage
+    * @param request Null not allowed
+    * @param callback Response write callback
+    * @param stream Response write stream
+    */
+    bool init(HttpTaskLoop* client, HttpRequest* request, write_callback callback, void* stream, write_callback headerCallback, void* headerStream, char* errorBuffer)
+    {
+        if (!m_pCurl)
+            return false;
+        if (!configureCURL(client, m_pCurl, errorBuffer))
+            return false;
+
+        /* get custom header data (if set) */
+        const vector<String>::type& headers = request->getHttpHeaders();
+        if (!headers.empty())
+        {
+            /* append custom headers one by one */
+            for (vector<String>::const_iterator it = headers.begin(); it != headers.end(); ++it)
+                m_pHeaders = curl_slist_append(m_pHeaders, it->c_str());
+            /* set custom headers for curl */
+            if (!setOption(CURLOPT_HTTPHEADER, m_pHeaders))
+                return false;
+        }
+        String cookieFilename = client->getCookieFilename();
+        if (!cookieFilename.empty())
+        {
+            if (!setOption(CURLOPT_COOKIEFILE, cookieFilename.c_str()))
+            {
+                return false;
+            }
+            if (!setOption(CURLOPT_COOKIEJAR, cookieFilename.c_str()))
+            {
+                return false;
+            }
+        }
+
+        return setOption(CURLOPT_URL, request->getUrl().c_str())
+            && setOption(CURLOPT_WRITEFUNCTION, callback)
+            && setOption(CURLOPT_WRITEDATA, stream)
+            && setOption(CURLOPT_HEADERFUNCTION, headerCallback)
+            && setOption(CURLOPT_HEADERDATA, headerStream);
+    }
+
+    /// @param responseCode Null not allowed
+    bool perform(long *responseCode)
+    {
+        if (CURLE_OK != curl_easy_perform(m_pCurl))
+            return false;
+        // 		CURLcode code = curl_easy_getinfo(m_pCurl, CURLINFO_RESPONSE_CODE, responseCode);
+        // 		if (code != CURLE_OK || !(*responseCode >= 200 && *responseCode < 300)) 
+        // 		{
+        // 			LogManager::getSingleton().stream(LML_NORMAL) 
+        // 				<< "Curl curl_easy_getinfo failed: " 
+        // 				<< curl_easy_strerror(code);
+        // 			return false;
+        // 		}
+        // Get some more data.
+
+        return true;
+    }
+};
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+//Process Get Request
+static int processGetTask(HttpTaskLoop* client, HttpRequest* request, write_callback callback, void* stream, long* responseCode, write_callback headerCallback, void* headerStream, char* errorBuffer)
+{
+    CURLRaii curl;
+    bool ok = curl.init(client, request, callback, stream, headerCallback, headerStream, errorBuffer)
+        && curl.setOption(CURLOPT_FOLLOWLOCATION, true)
+        && curl.perform(responseCode);
+    return ok ? 0 : 1;
+}
+//-----------------------------------------------------------------------
+//Process POST Request
+static int processPostTask(HttpTaskLoop* client, HttpRequest* request, write_callback callback, void* stream, long* responseCode, write_callback headerCallback, void* headerStream, char* errorBuffer)
+{
+    CURLRaii curl;
+    bool ok = curl.init(client, request, callback, stream, headerCallback, headerStream, errorBuffer)
+        && curl.setOption(CURLOPT_POST, 1)
+        && curl.setOption(CURLOPT_POSTFIELDS, request->getData())
+        && curl.setOption(CURLOPT_POSTFIELDSIZE, request->getDataSize())
+        && curl.perform(responseCode);
+    return ok ? 0 : 1;
+}
+//-----------------------------------------------------------------------
+//Process PUT Request
+static int processPutTask(HttpTaskLoop* client, HttpRequest* request, write_callback callback, void* stream, long* responseCode, write_callback headerCallback, void* headerStream, char* errorBuffer)
+{
+    CURLRaii curl;
+    bool ok = curl.init(client, request, callback, stream, headerCallback, headerStream, errorBuffer)
+        && curl.setOption(CURLOPT_CUSTOMREQUEST, "PUT")
+        && curl.setOption(CURLOPT_POSTFIELDS, request->getData())
+        && curl.setOption(CURLOPT_POSTFIELDSIZE, request->getDataSize())
+        && curl.perform(responseCode);
+    return ok ? 0 : 1;
+}
+//-----------------------------------------------------------------------
+//Process DELETE Request
+static int processDeleteTask(HttpTaskLoop* client, HttpRequest* request, write_callback callback, void* stream, long* responseCode, write_callback headerCallback, void* headerStream, char* errorBuffer)
+{
+    CURLRaii curl;
+    bool ok = curl.init(client, request, callback, stream, headerCallback, headerStream, errorBuffer)
+        && curl.setOption(CURLOPT_CUSTOMREQUEST, "DELETE")
+        && curl.setOption(CURLOPT_FOLLOWLOCATION, true)
+        && curl.perform(responseCode);
+    return ok ? 0 : 1;
+}
+//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
+HttpTaskLoop::HttpTaskLoop(const String& type, const String& name)
+    : TaskLoop(type, name)
+    , m_bKeepRunning(true)
+    , m_uTimeoutForConnect(30)
+    , m_uTimeoutForRead(60)
+{
+}
+//-----------------------------------------------------------------------
+HttpTaskLoop::~HttpTaskLoop()
+{
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::postTask(Task* task)
+{
+    _addToIncomingQueue(task);
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::postTaskAndReply(Task* task, Task* reply)
+{
+    PostTaskAndReplyRelay* pRelay = U2_NEW PostTaskAndReplyRelay(task, reply);
+    Task* pTask = TaskManager::getSingleton().createObject([=] { pRelay->Run(); });
+    _addToIncomingQueue(task);
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::run()
+{
+    m_thread = std::move(std::thread(std::bind(&HttpTaskLoop::_runInternal, this)));
+    m_thread.detach();
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::_runInternal()
+{
+    m_bKeepRunning = true;
+
+    for (;;)
+    {
+        Task* pTask = nullptr;
+        {
+            U2_LOCK_MUTEX(m_mtxIncomingQueue);
+            pTask = m_IncomingQueue.front();
+            if (pTask == nullptr)
+            {
+                U2_THREAD_WAIT(m_IncomingQueueSync, lck);
+                continue;
+            }
+            else
+            {
+                m_IncomingQueue.pop();
+            }
+        }
+
+        if (pTask != nullptr)
+        {
+            _runTask(pTask);
+        }
+
+        if (!m_bKeepRunning)
+            break;
+    }
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::quit()
+{
+    m_bKeepRunning = false;
+}
+//---------------------------------------------------------------------
+void HttpTaskLoop::_addToIncomingQueue(Task* task)
+{
+    U2_LOCK_MUTEX(m_mtxIncomingQueue);
+    bool bWasEmpty = m_IncomingQueue.empty();
+    m_IncomingQueue.push(task);
+    m_IncomingQueueSync.notify_all();
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::setTimeoutForConnect(size_t value)
+{
+    U2_LOCK_MUTEX(m_TimeoutForConnectMutex);
+    m_uTimeoutForConnect = value;
+}
+//-----------------------------------------------------------------------
+size_t HttpTaskLoop::getTimeoutForConnect()
+{
+    U2_LOCK_MUTEX(m_TimeoutForConnectMutex);
+    return m_uTimeoutForConnect;
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::setTimeoutForRead(size_t value)
+{
+    U2_LOCK_MUTEX(m_TimeoutForReadMutex);
+    m_uTimeoutForRead = value;
+}
+//-----------------------------------------------------------------------
+size_t HttpTaskLoop::getTimeoutForRead()
+{
+    U2_LOCK_MUTEX(m_TimeoutForReadMutex);
+    return m_uTimeoutForRead;
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::setSSLVerification(const String& caFile)
+{
+    U2_LOCK_MUTEX(m_SslCaFilenameMutex);
+    m_szSslCaFilename = caFile;
+}
+//-----------------------------------------------------------------------
+const String& HttpTaskLoop::getSSLVerification()
+{
+    U2_LOCK_MUTEX(m_SslCaFilenameMutex);
+    return m_szSslCaFilename;
+}
+//-----------------------------------------------------------------------
+void HttpTaskLoop::enableCookies(const String& cookieFile)
+{
+    U2_LOCK_MUTEX(m_CookieFilenameMutex);
+    if (cookieFile != BLANK)
+    {
+        m_szCookieFilename = cookieFile;
+    }
+    else
+    {
+        m_szCookieFilename = (FileUtils::getInstance()->getWritablePath() + "cookieFile.txt");
+    }
+}
+//-----------------------------------------------------------------------
+const String& HttpTaskLoop::getCookieFilename()
+{
+    U2_LOCK_MUTEX(m_CookieFilenameMutex);
+    return m_szCookieFilename;
+}
