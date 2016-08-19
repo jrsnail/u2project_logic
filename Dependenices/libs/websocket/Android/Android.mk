68f93a3f8d42bdfcdbea254c84f287354dd7b68d
jrsnail <jrsnail_cm@jirongdeMacBook-Pro.local>
2016-08-19 15:57:34 +0800

    * 修改Android.mk;

--- Dependenices/libs/websocket/Android/Android.mk
+++ Dependenices/libs/websocket/Android/Android.mk
@@ -3,6 +3,6 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_MODULE := u2_websocket_static
 LOCAL_MODULE_FILENAME := websocket
-LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebsocket.a
+LOCAL_SRC_FILES := $(TARGET_ARCH_ABI)/libwebsockets.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/../../../include
 include $(PREBUILT_STATIC_LIBRARY)
